77b39677682001d9a01c37552bb1aa14543bf0bb
Samuel Baraka <sababuvercetti@gmail.com>
2022-05-18 12:25:31 +0300

    Added breed model class

---
+++ Models/Breed.cs
@@ -0,0 +1,20 @@
+﻿
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+namespace ElGatoBackend.Models
+
+{
+    public class Breed
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int Id { get; set; }
+
+        [BsonElement("Name")]
+        public string BreedName { get; set; } = null!;
+        [BsonElement("Description")]
+        public string BreedDescription { get; set; }=null!;
+        [BsonElement("Image")]
+        public string BreedImage{ get; set; } = null!;
+    }
+}
