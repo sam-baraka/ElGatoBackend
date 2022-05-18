7caf506e7f7fc76be25adf16bf0468e1f9714cad
Samuel Baraka <sababuvercetti@gmail.com>
2022-05-18 12:53:20 +0300

    Added a breeds service

---
+++ Services/BreedsService.cs
@@ -0,0 +1,38 @@
+﻿using ElGatoBackend.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace BookStoreApi.Services;
+
+public class BreedsService
+{
+    private readonly IMongoCollection<Breed> _breedCollection;
+
+    public BreedsService(
+        IOptions<CatDatabaseSettings> catDatabaseSettings)
+    {
+        var mongoClient = new MongoClient(
+            catDatabaseSettings.Value.ConnectionString);
+
+        var mongoDatabase = mongoClient.GetDatabase(
+            catDatabaseSettings.Value.DatabaseName);
+
+        _breedCollection = mongoDatabase.GetCollection<Breed>(
+            catDatabaseSettings.Value.BreedsCollectionName);
+    }
+
+    public async Task<List<Breed>> GetAsync() =>
+        await _breedCollection.Find(_ => true).ToListAsync();
+
+    public async Task<Breed?> GetAsync(string id) =>
+        await _breedCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+    public async Task CreateAsync(Breed newBreed) =>
+        await _breedCollection.InsertOneAsync(newBreed);
+
+    public async Task UpdateAsync(string id, Breed updatedBreed) =>
+        await _breedCollection.ReplaceOneAsync(x => x.Id == id, updatedBreed);
+
+    public async Task RemoveAsync(string id) =>
+        await _breedCollection.DeleteOneAsync(x => x.Id == id);
+}
