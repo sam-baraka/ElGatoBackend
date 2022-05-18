d439b52cdbf1428e96383470384c331122399689
Samuel Baraka <sababuvercetti@gmail.com>
2022-05-18 12:35:04 +0300

    cat breeds database settings class

---
+++ Models/CatsDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿
+public class CatDatabaseSettings
+{
+    public string ConnectionString { get; set; } = null!;
+
+    public string DatabaseName { get; set; } = null!;
+
+    public string BreedsCollectionName { get; set; } = null!;
+}
